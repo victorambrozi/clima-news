3461abfb880e93aaf2b48f49f5069ec2182821b2
Victor Ambrozi <victorambrozi.dev@gmail.com>
2023-03-14 03:16:09 -0300

    inclui dados dinâmicos

--- src/js/domControl.js
+++ src/js/domControl.js
@@ -2,17 +2,100 @@ import { fetchAPI } from "./weather.js";
 import { controlChart } from "./chart.js";
 import { controlSwiper } from "./swiper.js";
 
-const submit = document.querySelector("#submit");
-
+const btnSearch = document.querySelector("#btn-search");
 
 const handleClick = async (event) => {
-    const city = document.querySelector("#search").value;
-    event.preventDefault();
+  const city = document.querySelector("#search").value;
+  const data = await fetchAPI(city);
+  const date = new Date(await data.current.date);
+  event.preventDefault();
 
-   const data = await fetchAPI(city);
-    console.log(data)
-    controlChart(data.current);
-}
+  // ambiente
+  const iconElement = document.querySelector('[data-forecast-current="icon"]');
+  const dateElement = document.querySelector('[data-forecast-current="date"]');
+  const tempElement = document.querySelector('[data-forecast-current="temp"]');
+  const tempMaxElement = document.querySelector(
+    '[data-forecast-current="temp-max"]'
+  );
+  const locationElement = document.querySelector(
+    '[data-forecast-current="location"]'
+  );
+  const sunsetElement = document.querySelector(
+    '[data-forecast-current="sunset"]'
+  );
+
+  const getDayForecast = (date) => {
+      let day = '';
+
+      switch (date.getDay()) {
+        case 1:
+          day = "Segunda";
+          break;
+        case 2:
+          day = "Terça";
+          break;
+        case 3:
+          day = "Quarta";
+          break;
+        case 4:
+          day = "Quinta";
+          break;
+        case 5:
+          day = "Sexta";
+          break;
+        case 6:
+          day = "Sábado";
+          break;
+        case 7:
+          day = "Domingo";
+          break;
+        default:
+            day = "Hoje";
+      }
+
+      return day;
+  }
+
+  iconElement.src = data.current.icon;
+  dateElement.textContent = `${getDayForecast(date).slice(0,3)}, ${date.getDay()}`;
+  tempElement.textContent = `${data.current.temp}°C`;
+  locationElement.textContent = `${data.current.location.name}, ${data.current.location.region}`
+  tempMaxElement.textContent = `${Math.trunc(data.current.tempMax)}°C`;
+  sunsetElement.textContent = `${data.current.sunset}`;
+  controlChart(data.current);
+//   FINAL HEADER
+
+// INIT MAIN
+ const forecastdayElement = data.forecastday.reduce((html, forecastday) => {
+  console.log(forecastday);
+    return html += `<li class="main-forecast-data-item">
+    <span class="main-forecast-data-item-dayweek">${forecastday.day}</span>
+    <div class="main-forecast-data-item-chain-of-rain">
+      <i class="fa-solid fa-droplet"></i>
+      
+      <span>54%</span>
+    </div>
+    
+    <div class="main-forecast-data-item-icon-situation-weather">
+      <i class="fa-solid fa-sun"></i>
+    </div>
+
+    <div class="main-forecast-data-item-chart-dayweek">
+      <span id="temp-min">22°C</span> <!-- min-->
+      <div class="main-forecast-data-item-chart-temp">
+        <div class="main-forecast-data-item-chart">
+          <span class="main-forcast-data-item-chart-progress"></span>
+        </div>
+        <!-- DESCOBRIR COMO FAZ O GRÁFICO PARA OS DOIS LADOS || ------|=====---- || -->
+      </div>
+      <span id="temp-max">40°C</span><!-- max-->
+    </div>
+  </li>`
+})
+console.log(forecastdayElement)
+
+// document.querySelector(".main-forecast-data-container").appendChild(forecastdayElement)
+};
 
 controlSwiper();
-submit.addEventListener("click", handleClick);
+btnSearch.addEventListener("click", handleClick);
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,7 +1,9 @@
 const API_KEY = "9f916719091d4adbb1c234509230703";
 
 export const fetchAPI = async (city) => {
-  const baseURL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city === null ? 'Salvador': city}&days=7&aqi=no&alerts=no&lang=pt_br`;
+  const baseURL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${
+    city === null ? "Salvador" : city
+  }&days=7&aqi=no&alerts=no&lang=pt_br`;
   const response = await fetch(baseURL);
   const data = await response.json();
 
@@ -10,15 +12,17 @@ export const fetchAPI = async (city) => {
 
   const currentDay = getForecastData((dayWeek) => dayWeek.hour)[0];
 
-  const forecastHour = [ // chance de chuva nos determinados horários
+  const forecastHour = [
+    // chance de chuva nos determinados horários
     currentDay[8].chance_of_rain,
     currentDay[12].chance_of_rain,
     currentDay[16].chance_of_rain,
     currentDay[20].chance_of_rain,
-    currentDay[23].chance_of_rain
-  ]
+    currentDay[23].chance_of_rain,
+  ];
 
-  const dataWeather = { // dados tratados para aplicação
+  const dataWeather = {
+    // dados tratados para aplicação
     current: {
       date: forecastdayOne.date,
       icon: forecastdayOne.day.condition.icon,
@@ -32,21 +36,23 @@ export const fetchAPI = async (city) => {
       precipitation: getForecastData((days) => days.day.daily_chance_of_rain),
       forecastHour,
     },
-    forecastday: {
-      day: getForecastData((day) => day.date),
-      percentPrecipitation: getForecastData((day) => day.day.totalprecip_mm), // DIVIDIR POR CEM E MULTIPLICAR POR CEM
-      temp: {
-        current: getForecastData((day) => day.day.avgtemp_c),
-        min: getForecastData((day) => day.day.mintemp_c),
-        max: getForecastData((day) => day.day.maxtemp_c),
+    forecastday: [
+      {
+        day: getForecastData((day) => day.date),
+        percentPrecipitation: getForecastData((day) => day.day.totalprecip_mm), // DIVIDIR POR CEM E MULTIPLICAR POR CEM
+        temp: {
+          current: getForecastData((day) => day.day.avgtemp_c),
+          min: getForecastData((day) => day.day.mintemp_c),
+          max: getForecastData((day) => day.day.maxtemp_c),
+        },
+        icon: getForecastData((day) => day.day.condition.icon),
+        chanceOfRain: getForecastData((day) => day.day.daily_chance_of_rain),
       },
-      icon: getForecastData((day) => day.day.condition.icon),
-      chanceOfRain: getForecastData((day) => day.day.daily_chance_of_rain),
-    },
+    ],
   };
 
   // salvar no localStorage
   localStorage.setItem("dataWeather", JSON.stringify(dataWeather));
-  
+
   return dataWeather;
 };
